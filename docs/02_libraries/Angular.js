975a1bf61f9708d15a5d3e75a412f83020d19531
MatsuraM <matsura@monotaro.com>
2016-07-20 08:33:55 +0900

    Angular.jsの説明を追加
    Angular.jsの説明を追加

---
+++ docs/02_libraries/Angular.js
@@ -0,0 +1,8 @@
+# Angular.js
+## 概要
+* Gooleによる、オープンソースのフレームワーク
+* MVCフレームワークである(View,Model,Controller)
+* リアルタイムでフォーム入力の即時表示する,入力チェック,ソート検索などが簡単にできる
+*　Reactive Programming:時間や外部の入力で変わる値や計算を、ユーザ自身がプログラムするのではなく、システム（言語自体やライブラリ）がユーザからは見えないところで反応してくれる。エクセルのようなイメージ
+* 公式ドキュメント:https://angularjs.org/(英語)
+　→日本語にしたもの(http://js.studio-kingdom.com/angularjs/)
