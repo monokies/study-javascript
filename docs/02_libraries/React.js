773e11f6d0b0efea9baab0b266578221f75e3d93
Katashu <whomeshion@gmail.com>
2016-07-20 08:53:24 +0900

    Create React.js

---
+++ docs/02_libraries/React.js
@@ -0,0 +1,19 @@
+React.js
+
+JavaScriptで書かれたライブラリ。react.jsをインクルードして使う。
+（MVCで言うところの）Viewのみを担当する。
+JavaScriptのコード中に（PHPの様に）「HTMLタグ(っぽいもの)」を書ける。
+
+return (
+
+  <div className="commentForm">
+
+    Hello, world! I am a CommentForm.
+
+  </div>
+
+);
+
+この「HTMLタグ（っぽいもの）」はJavaScriptのシンタックスではエラーになるので実行する前にJavascriptに変換する。
+変換は実行直前の自動変換も、事前の静的な変換も可能。前者はJSXTransformer.jsをインクルードすることで、後者はjsxコマンドで実施。
+jQueryと共存できる。
